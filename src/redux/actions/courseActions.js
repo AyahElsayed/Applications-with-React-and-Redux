b126c1aa580a15d979be57bba2c7c2738d64b3db
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-26 01:33:57 +0200

    action type constants

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,4 +1,6 @@
+import * as types from './actionTypes'
+
 export function createCourse(course) {
-    return { type: "CREATE_COURSE", course } // this object is an "action" 
+    return { type: types.CREATE_COURSE, course } // this object is an "action" 
     // so the function is called the "action creator"
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,9 +1,10 @@
 // Reducer:
 // function that accepts state and action and returns a new state
+import * as types from '../actions/actionTypes'
 
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case "CREATE_COURSE":
+        case types.CREATE_COURSE:
             // state.push(action.course) // don't use push 
             return [...state, { ...action.course }]
         // whatever is returned from the reducer, becomes the new state for that particular reducer
