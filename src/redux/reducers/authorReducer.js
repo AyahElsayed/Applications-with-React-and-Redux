4a002e88f30c9fe5ce51be4ccd8fe0dd3146a20f
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-27 16:40:40 +0200

    centralize initial Redux state

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,11 +1,12 @@
 // Reducer:
 // function that accepts state and action and returns a new state
 import * as types from '../actions/actionTypes'
+import initialState from './initialState'
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
     switch (action.type) {
         case types.LOAD_AUTHORS_SUCCESS:
-            return action.authors 
+            return action.authors
         default:
             return state
     }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,8 +1,9 @@
 // Reducer:
 // function that accepts state and action and returns a new state
 import * as types from '../actions/actionTypes'
+import initialState from './initialState'
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
     switch (action.type) {
         case types.CREATE_COURSE:
             // state.push(action.course) // don't use push 
