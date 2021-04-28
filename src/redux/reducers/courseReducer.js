49cdd92437e05d3231502dfa0ddc9cdc74046216
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-28 15:31:01 +0200

    handle creates and updates in Reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -5,10 +5,13 @@ import initialState from './initialState'
 
 export default function courseReducer(state = initialState.courses, action) {
     switch (action.type) {
-        case types.CREATE_COURSE:
-            // state.push(action.course) // don't use push 
+        case types.CREATE_COURSE_SUCCESS:
             return [...state, { ...action.course }]
         // whatever is returned from the reducer, becomes the new state for that particular reducer
+        case types.UPDATE_COURSE_SUCCESS:
+            return state.map(course =>
+                course.id === action.course.id ? action.course : course
+            );
         case types.LOAD_COURSES_SUCCESS:
             return action.courses
         default:
