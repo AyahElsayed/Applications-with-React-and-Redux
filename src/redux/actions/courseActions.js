bb2128213f971ac70adabd480169e4c7563de4a3
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-28 15:25:16 +0200

    Add save courses thunk and action creators

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,13 +1,9 @@
 import * as types from './actionTypes'
 import * as courseApi from '../../api/courseApi'
 
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course } // this object is an "action" 
-    // so the function is called the "action creator"
-}
-
 // First thunk
 
+// "action creator fuction"
 export function loadCourseSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses }
 }
@@ -20,4 +16,31 @@ export function loadCourses() {
                 throw error
             })
     }
+}
+
+// "action creator fuctions"
+export function createCourseSuccess(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+// Second thunk
+export function saveCourse(course) {
+    //getState lets us access the Redux store data
+    // eslint-disable-next-line no-unused-vars
+    return function (dispatch, getState) {
+        return courseApi
+            .saveCourse(course)
+            .then(savedCourse => {
+                course.id
+                    ? dispatch(updateCourseSuccess(savedCourse))
+                    : dispatch(createCourseSuccess(savedCourse));
+            })
+            .catch(error => {
+                throw error;
+            });
+    };
 }
